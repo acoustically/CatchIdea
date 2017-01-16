4ac32b2ad8d7d94cc02b74938336d2cd8905f91f
acoustically <acoustically@kookmin.ac.kr>
2017-01-17 05:15:31 +0900

    implement authenticate

--- CatchIdea/db/schema.rb
+++ CatchIdea/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104210348) do
+ActiveRecord::Schema.define(version: 20170116162951) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
+    t.integer  "permission"
   end
 
 end
