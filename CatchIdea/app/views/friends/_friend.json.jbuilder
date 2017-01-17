afeec907ad02316cf0fc64c572023c4a3f9c1ad2
acoustically <acoustically@kookmin.ac.kr>
2017-01-18 03:27:13 +0900

    add friend_model

---
+++ CatchIdea/app/views/friends/_friend.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! friend, :id, :user_id, :name, :email, :created_at, :updated_at
+json.url friend_url(friend, format: :json)
--- CatchIdea/db/schema.rb
+++ CatchIdea/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116162951) do
+ActiveRecord::Schema.define(version: 20170117174745) do
+
+  create_table "friends", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
