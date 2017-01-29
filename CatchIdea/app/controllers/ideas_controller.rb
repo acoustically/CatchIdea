aa967d09cbc8e9ec5af5493a0559e1ac0ef88277
acoustically <acoustically@kookmin.ac.kr>
2017-01-29 22:20:28 +0900

    delete edit link

--- CatchIdea/app/controllers/ideas_controller.rb
+++ CatchIdea/app/controllers/ideas_controller.rb
@@ -39,7 +39,7 @@ class IdeasController < ApplicationController
 
     respond_to do |format|
       if @idea.save
-        format.html { redirect_to @idea, notice: 'Idea was successfully created.' }
+        format.html { redirect_to controller: :ideas, action: :index, notice: 'Idea was successfully created.' }
         format.json { render :show, status: :created, location: @idea }
       else
         format.html { render :new }
